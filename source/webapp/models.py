50ed7870a8ae908747773f5fcfeceb13affe0808
Emil <emilrakaev@gmail.com>
2021-01-23 12:50:54 +0600

    Добавил модель Friend в webapp и зарегестрировал ее в админке

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Friend(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='user_friends',
+                             verbose_name='Пользователь')
+    friend = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='friend_friends',
+                             verbose_name='Друг')
+
+    def __str__(self):
+        return f'{self.user} - {self.friend}'
+
+    class Meta:
+        verbose_name = 'Друг'
+        verbose_name_plural = 'Друзья'
+

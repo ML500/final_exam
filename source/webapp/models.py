ad81c9653b07c5723b56241dc76f6edb47e17e7e
Emil <emilrakaev@gmail.com>
2021-01-23 15:14:22 +0600

    Добавил модель сообщение

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,12 +1,17 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
+MESSAGE_STATUS_CHOICES = (
+    ('inbox', 'Входящие'),
+    ('outbox', 'Исходящие')
+)
+
 
 class Friend(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='user_friends',
                              verbose_name='Пользователь')
     friend = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='friend_friends',
-                             verbose_name='Друг')
+                               verbose_name='Друг')
 
     def __str__(self):
         return f'{self.user} - {self.friend}'
@@ -15,3 +20,17 @@ class Friend(models.Model):
         verbose_name = 'Друг'
         verbose_name_plural = 'Друзья'
 
+
+class Message(models.Model):
+    froms = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='from_messages',
+                              verbose_name='Сообщение')
+    whom = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='whom_messages',
+                             verbose_name='Сообщение')
+    text = models.TextField(max_length=1000, blank=True, null=True, verbose_name='Текст сообщения')
+
+    def __str__(self):
+        return f'{self.froms} - {self.whom}'
+
+    class Meta:
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщения'
